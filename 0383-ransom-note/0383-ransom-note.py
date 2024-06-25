3ccdc515b8653945cc76cd5daabe301f206a6e38
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-06-25 18:07:03 +0900

    Time: 70 ms (23.04%), Space: 11.7 MB (89.32%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -9,7 +9,12 @@ class Solution(object):
         ransom_c = Counter(ransomNote)
         magazine_c = Counter(magazine)
 
+        '''
         for char, n in ransom_c.items():
             if magazine_c[char] < n: ## 하나라도 안되는 게 있으면 False
                 return False
         return True
+        '''
+
+        return ransom_c & magazine_c == ransom_c
+        # &: 교집합 역할을 함. 교집합이 ransom_c와 같다는 것은 곧 ransom_c가 magazine_c에 고대로 있다는 뜻
