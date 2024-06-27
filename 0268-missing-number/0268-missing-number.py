b76c6060f3efa82a7fdc3361886adbf15cd51209
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-06-27 19:13:26 +0900

    Time: 92 ms (76.52%), Space: 12.9 MB (62.63%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,11 +1,7 @@
 class Solution(object):
     def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        # my solution
         nums.sort()
-
         i=0
         while nums[i] == i and i<len(nums)-1:
             i += 1
@@ -13,3 +9,22 @@ class Solution(object):
             return len(nums)
         else: 
             return i
+            
+        '''
+        # other solution 1 : using the sum
+        target_sum = len(nums)*(len(nums)+1) / 2
+        my_sum = sum(nums)
+
+        return target_sum - my_sum
+
+        # other solution 2 : three cases
+        nums.sort()
+        if nums[0] != 0:
+            return 0
+        if nums[-1] != len(nums):
+            return len(nums)
+        for i in range(1, len(nums)):
+            if nums[i] != i:
+                return i
+        return 0 
+        '''
