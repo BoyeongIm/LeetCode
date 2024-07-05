9d88ced3e9e9b0579aa918ca16634be797d8bf4f
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-05 11:40:04 +0900

    Time: 0 ms (100%), Space: 11.8 MB (45.52%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,8 +12,9 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q) return true;
+        if (!p && !q) return true; // !p||!q 보다 앞에 있어야 트리를 다 돌았을 때 제대로 true를 return할 수 있음.
         if (!p || !q) return false;
+        // 여기까지의 조건에 안걸렸다면, 구조적으로는 같은 것이기 때문에 value 비교
         if (p->val != q->val) return false;
         
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
