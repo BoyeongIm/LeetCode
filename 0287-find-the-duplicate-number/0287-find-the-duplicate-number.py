2a9488d5a03098ec31303ac74219be592838abbc
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-12 21:57:21 +0900

    Time: 579 ms (11.56%), Space: 23.1 MB (98.96%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -4,17 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #nums.sort()
-        start, end = 0, len(nums)-1
-        while start < end:
-            if nums[start] == nums[end]:
-                break
-            elif nums[start] in nums[start+1:]:
-                break
-            elif nums[end] in nums[:end]:
-                start = end
-                break
-            else:
-                start+=1
-
-        return nums[start]
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] == nums[i+1]:
+                return nums[i]
