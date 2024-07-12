512e691c4f4b3181a027dd6f1bcd13a0db7947af
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-12 21:23:40 +0900

    Time: 7 ms (82.41%), Space: 18.3 MB (5.59%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -5,13 +5,13 @@ public:
         int end = numbers.size()-1;
 
         while (start < end) {
-            if (numbers[start]+numbers[end] > target) {
-                end--;
+            if (numbers[start]+numbers[end] == target) {
+                break;
             }
             else if (numbers[start]+numbers[end] < target) {
                 start++;
             }
-            else break;
+            else end--;
         }
 
         return {start+1, end+1};
