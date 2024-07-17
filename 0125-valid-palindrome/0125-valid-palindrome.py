cb6da665f9a272fe4a1a7b48dcdce54450d5dee9
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-17 12:13:31 +0900

    Time: 322 ms (8.15%), Space: 16.1 MB (5.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -12,13 +12,13 @@ class Solution(object):
             if (s[i]>='0' and s[i]<='9') or (s[i]>='a' and s[i]<='z') or (s[i]>='A' and s[i]<='Z'):
                 new_str += s[i].lower()
         
-        if new_str == new_str[::-1]:
-            return True
+        if new_str != new_str[::-1]:  # new_str의 전체를 "거꾸로" 읽어라!
+            return False
+
+        return True
 
         """
         for j in range(len(new_str)//2):
             if new_str[j] != new_str[len(new_str)-1-j]:
                 return False
         """
-
-        return False
