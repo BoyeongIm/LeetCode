2f283912970a94fc3fcb5bee3e2262dc4d6ca95d
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-17 12:07:11 +0900

    Time: 317 ms (8.97%), Space: 16.7 MB (5.49%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s) == 1:
+            return True;
+
+        s = s.lower()
+        new_str = ""
+        for i in range(len(s)):
+            if (s[i]>='0' and s[i]<='9') or (s[i]>='a' and s[i]<='z'):
+                new_str += s[i]
+        print(len(new_str)//2)
+        for j in range(len(new_str)//2):
+            if new_str[j] != new_str[len(new_str)-1-j]:
+                return False
+
+        return True
