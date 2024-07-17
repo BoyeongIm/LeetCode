eb060f572bf605bd5cc3ca60d8973e4fb493fd49
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-17 11:50:15 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if (s.length() == 1) return true;
+
+        string new_str;
+        for (int i=0; i<s.length(); i++) {
+            if (isalpha(s[i])) {
+                new_str += tolower(s[i]);
+            }
+        }
+        
+        for (int j=0; j<new_str.length()/2; j++) {
+            if (new_str[j] != new_str[new_str.length()-1-j]) return false;
+        }
+
+        return true;
+    }
+};
