7f7615fdccc891b5d92b3641089dea6adf8026cf
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-18 17:35:31 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        if (r * c != mat.size() * mat[0].size())
+        int org_r = mat.size();
+        int org_c = mat[0].size();
+        if (r * c != org_r*org_c)
             return mat;
-        if (r == mat.size() && c == mat[0].size())
+        if (r == org_r && c == org_c)
             return mat;
 
+        vector<vector<int>> new_mat(r, vector<int>(c,0));
+
+        int row = 0;
+        int col = 0;
+
+        for (int i=0; i<org_r; i++) {
+            for (int j=0; j<org_c; j++) {
+                new_mat[row][col] = mat[i][j];
+                col ++;
+
+                if (col==j) {
+                    row++;
+                    col=0; // 다음 row로 넘어가면서 col이 초기화되어야 함.
+                }
+            }
+        }
+        
+        return new_mat;
+        
+        /*
         vector<int> to_1d;
         for (int i = 0; i < mat.size(); i++) {
             for (int j = 0; j < mat[0].size(); j++) {
@@ -22,6 +44,6 @@ public:
             new_mat.push_back(row);
         }
 
-        return new_mat;
+        return new_mat;*/
     }
 };
