2f8d64026ee117bbb83a2368bd933235a8b94579
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-18 17:33:45 +0900

    Time: 6 ms (75.19%), Space: 13.9 MB (7.46%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        if (r*c != mat.size()*mat[0].size()) return mat;
-        if (r==mat.size() && c==mat[0].size()) return mat;
+        if (r * c != mat.size() * mat[0].size())
+            return mat;
+        if (r == mat.size() && c == mat[0].size())
+            return mat;
 
         vector<int> to_1d;
-        for (int i=0; i<mat.size(); i++) {
-            for (int j=0; j<mat[0].size(); j++) {
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[0].size(); j++) {
                 to_1d.push_back(mat[i][j]);
             }
         }
-        
+
         vector<vector<int>> new_mat;
-        for (int i=0; i<r; i++) {
+        for (int i = 0; i < r; i++) {
             vector<int> row(c);
-            for (int j=0; j<c; j++) {
-                row[j] = to_1d[c*i+j];
+            for (int j = 0; j < c; j++) {
+                row[j] = to_1d[c * i + j];
             }
             new_mat.push_back(row);
         }
