723eed84a2890dc7829724dd03ab1c92eb8bc7e2
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-22 10:44:05 +0900

    Time: 36 ms (31.48%), Space: 27.3 MB (39.87%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        map<int, string> hname;
+        for (int i = 0; i < names.size(); i++) {
+            hname.insert(make_pair(heights[i], names[i]));
+        }
+        vector<string> sorted_result;
+        for (auto it = hname.rbegin(); it != hname.rend(); it++) {
+            sorted_result.push_back(it->second);
+        }
+        return sorted_result;
+    }
+};
