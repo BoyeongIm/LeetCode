5ac6405f14077eeccf54a835bd9949446f5179cb
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-26 15:41:26 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        if (k==0) {
+            for (const auto& e : flights) {
+                if (e[0]==src && e[1]==dst) return e[2];
+            }
+        }
+        priority_queue<tuple<int, int, int>, vector<tuple<int,int,int>>, greater<tuple<int,int, int>>> prQ;
+        prQ.push({0, src, 0}); // cost, node, stops
+        vector<vector<int>> dist(n, vector<int>(k+2, 10001)); // distance & stop
+        dist[src][0] = 0;
+        
+        while (!prQ.empty()) {
+            auto [cost, node, stops] = prQ.top();
+            prQ.pop();
+
+            if (stops > k) continue;
+            
+            for (const auto& e : flights) {
+                if (e[0]==node) {
+                    int to = e[1];
+                    int w = e[2];
+                    if (dist[node][stops] + w < dist[to][stops+1]) {
+                        dist[to][stops+1] = cost + w;
+                        prQ.push({cost+w, to, stops+1});                        
+                    }                    
+                }
+            }
+        }
+        int result = INT_MAX;
+        for (int i=0; i<=k+1; i++) {
+            result = min(result, dist[dst][i]);
+        }
+        
+        if (result < INT_MAX) return result;
+        else return -1;
+    }
+};
