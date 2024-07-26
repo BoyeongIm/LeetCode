e3e402b854aaaa8f7670b180be3f17864b07ddd9
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-26 12:57:09 +0900

    Time: 23 ms (80.88%), Space: 15.6 MB (65.01%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     static bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
-        if (a.second==b.second) return a.first > b.first;
+        if (a.second == b.second)
+            return a.first > b.first;
         return a.second < b.second;
     }
 
@@ -9,10 +10,10 @@ public:
         vector<vector<int>> distance(n, vector<int>(n, 10001));
         map<int, int> threshold_freq;
 
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             distance[i][i] = 0;
         }
-        
+
         for (const auto& edge : edges) {
             int start = edge[0];
             int end = edge[1];
@@ -21,26 +22,28 @@ public:
             distance[end][start] = weight;
         }
 
-        for (int k=0; k<n; k++) {
-            for (int i=0; i<n; i++) {
-                for (int j=0; j<n; j++) {
-                    if (distance[i][k]+distance[k][j] < distance[i][j]) {
-                        distance[i][j] = distance[i][k]+distance[k][j];
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (distance[i][k] + distance[k][j] < distance[i][j]) {
+                        distance[i][j] = distance[i][k] + distance[k][j];
                     }
                 }
             }
         }
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (distance[i][j] <= distanceThreshold) {
-                    if (threshold_freq.find(i)!=threshold_freq.end())
+                    if (threshold_freq.find(i) != threshold_freq.end())
                         threshold_freq[i]++;
-                    else threshold_freq[i]=1;
+                    else
+                        threshold_freq[i] = 1;
                 }
             }
         }
 
-        vector<pair<int, int>> threshold_freq2(threshold_freq.begin(), threshold_freq.end());
+        vector<pair<int, int>> threshold_freq2(threshold_freq.begin(),
+                                               threshold_freq.end());
         sort(threshold_freq2.begin(), threshold_freq2.end(), cmp);
         return threshold_freq2.begin()->first;
     }
