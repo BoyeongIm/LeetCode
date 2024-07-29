352483c16a40154a0f2ec01aea920dfc70a1f9bd
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-29 21:58:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,23 @@
+class Solution(object):   
+    def searchInsert(self, nums, target):
+        if target > nums[-1]:
+            return len(nums)
+        elif target < nums[0]:
+            return 0
+        elif target == nums[0]:
+            return 0
+
+        start, end = 0, len(nums)-1 
+
+        while start<end:
+            mid = (start+end)//2
+            if nums[mid] < target:
+                start = mid+1
+            else:
+                end = mid-1
+        if nums[start]==target:
+            return start
+        elif nums[start]<target:
+            return start+1
+        else:
+            return end-1
