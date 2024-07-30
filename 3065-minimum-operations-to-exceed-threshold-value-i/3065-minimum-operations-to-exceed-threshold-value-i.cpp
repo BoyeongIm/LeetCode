3439ce1a6cea7eca0719c1cbc5ca77fe8c07f0c4
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-30 11:41:52 +0900

    Time: 0 ms (100%), Space: 24.4 MB (11.14%) - LeetHub

---
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int count = 0;
+        for (auto n : nums) {
+            if (n < k) count++;
+        }
+        return count;
+    }
+};
