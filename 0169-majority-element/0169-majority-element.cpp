3b19f2da3bba747fc724c17aadf3ed946dfb01de
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-07-30 16:42:36 +0900

    Time: 24 ms (16.02%), Space: 28.2 MB (7.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,8 +4,7 @@ public:
         unordered_map<int, int> freq;
         int majority = 0;
         for (auto n:nums) {
-            if (freq.count(n)) freq[n]+=1;
-            else freq[n]=1;
+            freq[n]++;
 
             if (freq[n] > nums.size()/2) {
                 majority = n;
