f8451f0e640a555343aef592083e77a8bea7a2ae
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-01 13:57:58 +0900

    Time: 11 ms (91.51%), Space: 12 MB (19.77%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,12 +1,13 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
+        print(min(strs))
         lensorted = sorted(strs, key=len)
         standard = lensorted[0]
         count = 0
         while count<len(standard):
             for str in lensorted[1:]:
-                if standard[0:count+1] != str[0:count+1]:
-                    return standard[0:count] 
+                if standard[:count+1] != str[:count+1]:
+                    return standard[:count] 
             count+=1
         return standard
                     
