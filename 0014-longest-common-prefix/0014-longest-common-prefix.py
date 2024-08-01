aa0489de130ee8e45e64cb124e6b75f5c3a61626
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-01 13:57:28 +0900

    Time: 23 ms (15.88%), Space: 11.7 MB (83.17%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,14 +1,10 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
         lensorted = sorted(strs, key=len)
         standard = lensorted[0]
         count = 0
         while count<len(standard):
-            for str in strs:
+            for str in lensorted[1:]:
                 if standard[0:count+1] != str[0:count+1]:
                     return standard[0:count] 
             count+=1
