00c415d0fb50bc91cad3e5a2624a0cb030885ad2
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-02 14:53:31 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.size()==1) return 1;
-        for (int i=0; i<nums.size()-1; i++) {
+        if (nums.size()==2 && nums[0]==nums[1]) return 1; 
+
+        int i = 0;
+        while (i<nums.size()-1) {
             if (nums[i]==nums[i+1]) {
                 int idx = i+1;
-                while (nums[idx]==nums[i] && idx<nums.size()) 
-                    nums.erase(nums.begin()+(idx));
+                while (nums[idx]==nums[i] && idx<nums.size()) {
                     idx++;
-            }
+                }
+                nums.erase(nums.begin()+i+1, nums.begin()+idx);
+            } else i++;            
         }
+
         return nums.size();
     }
 };
