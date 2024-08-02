fb532ff74ede2e39fb8e683b26e2068555b274f0
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-02 14:12:32 +0900

    Time: 57 ms (5.16%), Space: 21.4 MB (14.2%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         for (int i=0; i<nums.size()-1; i++) {
             if (nums[i]==nums[i+1]) {
                 int idx = i+1;
-                while (nums[idx]==nums[i]) 
+                while (nums[idx]==nums[i] && idx<nums.size()) 
                     nums.erase(nums.begin()+(idx));
                     idx++;
             }
