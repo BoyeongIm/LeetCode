6a4a266697f4ebd09b3ce79131f051ca4d7f0432
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-03 15:40:11 +0900

    Time: 72 ms (9.66%), Space: 11.9 MB (13.1%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
@@ -5,8 +5,22 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
+        target_dict=dict()
         for t in target:
             if t not in arr:
                 return False
-        return True
+            if t not in target_dict:
+                target_dict[t]=1
+            else:
+                target_dict[t]+=1
+
+        arr_dict = dict()
+        for n in arr:
+            if n not in arr_dict:
+                arr_dict[n]=1
+            else:
+                arr_dict[n]+=1
+        
+        return target_dict==arr_dict
+
         
