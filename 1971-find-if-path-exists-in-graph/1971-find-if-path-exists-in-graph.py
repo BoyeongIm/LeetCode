4e3feeca5c9b3c9dbf959b03227e00e937388f0c
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-05 14:14:39 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -4,22 +4,19 @@ class Solution(object):
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)
-
         path, visited = [], set()
-        self.dfs(n, adj, source, destination, path, visited)
-        if path and path[-1]==destination:
-            print(path)
+        self.dfs(adj, source, destination, path, visited)
+        if path:
             return True
         else: 
-            print(path)
             return False
-
-    def dfs(self, n, adj, source, destination, path, visited):
-        if source in visited:
-            return
-        visited.add(source)
-        path.append(source)
-        for neighbor in adj[source]:
-            if neighbor not in visited:
-                self.dfs(n, adj, neighbor, destination, path, visited)
-                
+        
+    def dfs(self, adjlist, node, destination, path, visited):
+        if node not in visited:
+            visited.add(node)
+            path.append(node)
+            for neighbor in adjlist[node]:
+                self.dfs(adjlist, neighbor, destination, path, visited)
+                if neighbor==destination:
+                    return
+            path.pop()
