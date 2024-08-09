d0f68df750ce26644553e38345df076e233519b3
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-09 13:46:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findPermutationDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: int
+        """
+        s = "abc"
+        print(s.index("c"))
+        return 0
