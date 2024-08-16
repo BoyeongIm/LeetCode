481b40cccbb7d89ff9e85b67a75e9f8ade0428a1
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-16 12:24:51 +0900

    Time: 0 ms (100%), Space: 16.8 MB (65.5%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<string> dfs(TreeNode*& currNode, vector<string>& paths, string path) {
+    void dfs(TreeNode*& currNode, vector<string>& paths, string path) {
         if (!currNode->left && !currNode->right) {
             if (find(paths.begin(), paths.end(), path) == paths.end()) {
                 paths.push_back(path);
@@ -9,12 +9,12 @@ public:
 
         if (currNode->left) dfs(currNode->left, paths, path+"->"+to_string(currNode->left->val));
         if (currNode->right) dfs(currNode->right, paths, path+"->"+to_string(currNode->right->val));
-
-        return paths;
     }
+
     vector<string> binaryTreePaths(TreeNode* root) {
         if (!root) return {};
         vector<string> paths;
-        return dfs(root, paths, to_string(root->val));
+        dfs(root, paths, to_string(root->val));
+        return paths;
     }
 };
