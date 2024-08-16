09159bece3e67eafede3504f3623fcbdcd574590
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-16 10:45:36 +0900

    Time: 27 ms (93.79%), Space: 11.6 MB (93.14%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,17 @@
 class Solution(object):
     def combinationSum(self, candidates, target):
         combinations = []
-        candidates.sort()
+        #candidates.sort()
         self.__help(candidates, combinations, [], target)
         return combinations
     
     def __help(self, candidates, combinations, comb, target):
         if target==0:
-            comb.sort()
-            if comb not in combinations:
-                combinations.append(comb)
-                return
+            combinations.append(comb)
+            return
         
         for i in range(len(candidates)):
             if target-candidates[i]<0:
                 continue
-            self.__help(candidates, combinations, comb+[candidates[i]], target-candidates[i])     
+            self.__help(candidates[i:], combinations, comb+[candidates[i]], target-candidates[i])     
         
