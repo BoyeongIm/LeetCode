8fc08b29dd30bef447e35b750b5233aaae8997c3
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-16 11:54:10 +0900

    Time: 18 ms (38.92%), Space: 11.6 MB (89.68%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -1,27 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def binaryTreePaths(self, root):
         if not root:
             return []
         paths = []
-        self.__help(root, paths, [root.val])
+        self.__help(root, paths, str(root.val))
         return paths
     
     def __help(self, currNode, paths, path):
         if not currNode.left and not currNode.right:
-            pathstring = str(path[0])
-            for i in range(1, len(path)):
-                pathstring += "->"
-                pathstring += str(path[i])
-            if pathstring not in paths:
-                paths.append(pathstring)
+            if path not in paths:
+                paths.append(path)
                 return
         if currNode.left:
-            self.__help(currNode.left, paths, path+[currNode.left.val])
+            self.__help(currNode.left, paths, path+"->"+str(currNode.left.val))
         if currNode.right:
-            self.__help(currNode.right, paths, path+[currNode.right.val])
+            self.__help(currNode.right, paths, path+"->"+str(currNode.right.val))
