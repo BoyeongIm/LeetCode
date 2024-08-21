59dc69cef2c9966da443f61e8ab394ecd4392918
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-21 22:03:10 +0900

    Time: 131 ms (42.93%), Space: 41.5 MB (56.7%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -1,24 +1,34 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution(object):
     def getIntersectionNode(self, headA, headB):
         curr1 = headA
         curr2 = headB
-        l1 = []
-        l2 = []
-        while curr1:   
-            l1.append(curr1)
+        alength = 0
+        blength = 0
+        while curr1:
+            alength+=1
             curr1 = curr1.next
         while curr2:
-            l2.append(curr2)
+            blength+=1
             curr2 = curr2.next
-        for i in range(len(l1)):
-            for j in range(len(l2)):
-                if l1[i]==l2[j]:
-                    return l1[i]
         
-        return None
+        currA = headA
+        currB = headB
+        if alength > blength:
+            n = alength-blength
+            for i in range(n):
+                currA = currA.next
+            while currA!=currB and (currA.next and currB.next):
+                currA = currA.next
+                currB = currB.next
+        else:
+            n = blength-alength
+            for i in range(n):
+                currB = currB.next
+            while currA!=currB and (currA.next and currB.next):
+                currA = currA.next
+                currB = currB.next
+        
+        if currA==currB:
+            return currA
+        else:
+            return None
