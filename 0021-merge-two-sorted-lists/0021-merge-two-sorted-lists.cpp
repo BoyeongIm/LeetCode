b1f9ad2bf7c25818b014d68e566421c2943263c7
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-22 00:32:52 +0900

    Time: 4 ms (63.81%), Space: 19.8 MB (10.98%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,23 +1,22 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if (!list1) return list2;
         if (!list2) return list1;
 
-        ListNode* newH = new ListNode();
-        ListNode* temp = newH;
         ListNode* curr1 = list1;
         ListNode* curr2 = list2;
+        ListNode* newHead = new ListNode();
+
+        if (curr1->val < curr2->val) {
+            newHead = curr1;
+            curr1 = curr1->next;
+        } else {
+            newHead = curr2;
+            curr2 = curr2->next;
+        }
+
+        ListNode* temp = newHead;
 
         while (curr1 && curr2) {
             if (curr1->val < curr2->val) {
@@ -33,6 +32,6 @@ public:
         if (curr1) temp->next = curr1;
         if (curr2) temp->next = curr2;
 
-        return newH->next;
+        return newHead;
     }
 };
