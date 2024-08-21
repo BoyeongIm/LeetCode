b493ff153c1f270a0315b8899b80a5e19490f336
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-08-22 00:33:39 +0900

    Time: 7 ms (39.58%), Space: 19.7 MB (39.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -4,33 +4,31 @@ public:
         if (!list1) return list2;
         if (!list2) return list1;
 
-        ListNode* curr1 = list1;
-        ListNode* curr2 = list2;
-        ListNode* newHead = new ListNode();
+        ListNode* newHead = nullptr;
 
-        if (curr1->val < curr2->val) {
-            newHead = curr1;
-            curr1 = curr1->next;
+        if (list1->val < list2->val) {
+            newHead = list1;
+            list1 = list1->next;
         } else {
-            newHead = curr2;
-            curr2 = curr2->next;
+            newHead = list2;
+            list2 = list2->next;
         }
 
-        ListNode* temp = newHead;
-
-        while (curr1 && curr2) {
-            if (curr1->val < curr2->val) {
-                temp->next = curr1;
-                curr1 = curr1->next;
+        ListNode* tmp = newHead;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                tmp->next = list1;
+                list1 = list1->next;
             } else {
-                temp->next = curr2;
-                curr2 = curr2->next;
+                tmp->next = list2;
+                list2 = list2->next;
             }
-            temp = temp->next;
+            
+            tmp = tmp->next;
         }
 
-        if (curr1) temp->next = curr1;
-        if (curr2) temp->next = curr2;
+        if (list1) tmp->next = list1;
+        if (list2) tmp->next = list2;
 
         return newHead;
     }
