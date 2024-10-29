88ef0d93d7af06d914645df7993f11812c9b75cb
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-10-29 12:19:02 +0900

    Time: 0 ms (100%), Space: 19.3 MB (81.81%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,14 +6,17 @@
 #         self.right = right
 
 class Solution(object):
-    def _DFT_inorderHelp(self, currNode, treelist):
+    def _DFT_inorderHelp(self, currNode, k):
         if not currNode:
             return 
-        self._DFT_inorderHelp(currNode.left, treelist)
-        treelist.append(currNode.val)
-        self._DFT_inorderHelp(currNode.right, treelist)
+        left = self._DFT_inorderHelp(currNode.left, k)
+        if left is not None:
+            return left
+        self.order += 1
+        if self.order == k:
+            return currNode.val
+        return self._DFT_inorderHelp(currNode.right, k)
 
     def kthSmallest(self, root, k):
-        treelist = []
-        self._DFT_inorderHelp(root, treelist)
-        return treelist[k-1]
+        self.order = 0
+        return self._DFT_inorderHelp(root, k)
