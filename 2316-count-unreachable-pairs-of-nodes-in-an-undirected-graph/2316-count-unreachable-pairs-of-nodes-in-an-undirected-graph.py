1586528f17a79f1f750cdb09e334de1c4217cb14
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-10-31 11:49:50 +0900

    Time: 355 ms (57.14%), Space: 57.7 MB (93.75%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -1,19 +1,36 @@
 class Solution(object):
     def countPairs(self, n, edges):
-        adj = {i:[] for i in range(n)}
-        for u,v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-        print(adj)
-        total = n*(n-1) / 2
-        comb = set()
+        #adj = {i:[] for i in range(n)}
+        #for u,v in edges:
+        #    adj[u].append(v)
+        #    adj[v].append(u)
+        parents = [i for i in range(n)]
+        for u, v in edges:
+            if self.findparent(u, parents) != self.findparent(v, parents):
+                self.unite(u, v, parents)
+        count = {i:0 for i in range(n)}
         for i in range(n):
-            if len(adj[i]) == 1:
-                comb.add(tuple(sorted((i, adj[i][0]))))
-            elif len(adj[i]) > 1:
-                for j in range(len(adj[i])):
-                    comb.add(tuple(sorted((i,adj[i][j]))))
-                    for k in adj[i][j+1:]:
-                        comb.add(tuple(sorted((adj[i][j],k))))
-        total -= len(comb)
+            parents[i] = self.findparent(i, parents)
+            count[parents[i]] +=1
+        total = n*(n-1)/2
+        for c in count:
+            if count[c] > 1:
+                v = count[c]
+                total -= v*(v-1)/2
         return total
+
+    def findparent(self, node, parents):
+        if node != parents[node]:
+            parents[node] = self.findparent(parents[node], parents)
+        return parents[node]
+    
+    def unite(self, u, v, parents):
+        u_root = self.findparent(u, parents)
+        v_root = self.findparent(v, parents)
+        if u_root == v_root:
+            return
+        if u_root < v_root:
+            parents[v_root] = u_root
+        else:
+            parents[u_root] = v_root
+    
