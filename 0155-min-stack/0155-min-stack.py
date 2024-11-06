2904569c004dc9ab5a28437ea0304be1b922c81c
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-07 00:23:41 +0900

    Time: 300 ms (14.52%), Space: 15.3 MB (53.18%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,27 +1,19 @@
 class MinStack(object):
     def __init__(self):
         self.stack = []
-        self.minv = []
 
     def push(self, val):
         self.stack.append(val)
-        if not self.minv:
-            self.minv.append(val)
-        if val < self.minv[0]:
-            self.minv[0] = val
 
     def pop(self):
-        self.stack.pop(-1)
-        # del 함수의 time complexity: O(n)
+        del self.stack[-1]
 
     def top(self):
         return self.stack[-1]
 
     def getMin(self):
-        # return min(self.stack)
-        # min 메서드의 time complexity: O(n)
-        return self.minv[0]
-
+        return min(self.stack)
+        
 
 
 # Your MinStack object will be instantiated and called as such:
