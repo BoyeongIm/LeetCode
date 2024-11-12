9a1fae12dad90ac2afde8408dd5386d8481e3d28
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-12 18:09:59 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,9 @@
 class Solution(object):
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        if len(s) == 1:
-            return True;
-
-        new_str = ""
-        for i in range(len(s)):
-            if (s[i]>='0' and s[i]<='9') or (s[i]>='a' and s[i]<='z') or (s[i]>='A' and s[i]<='Z'):
-                new_str += s[i].lower()
-        
-        if new_str != new_str[::-1]:  # new_str의 전체를 "거꾸로" 읽어라!
-            return False
-
-        return True
-
-        """
-        for j in range(len(new_str)//2):
-            if new_str[j] != new_str[len(new_str)-1-j]:
-                return False
-        """
+        s = s.lower()
+        combined = ''
+        for w in s:
+            if  w >= 'a' and w <= 'z':
+                combined += w
+            elif w >= '0' and w <= '9':
+        return combined == combined[::-1]
