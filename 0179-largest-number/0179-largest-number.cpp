7cafc2cefe6d37794aaf63e7fccbd0b0e9cb7036
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-18 19:45:06 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static string repeatString(string& s, int n) {
+        string result = "";
+        for(int i=0; i<n; i++) {
+            result += s;
+        }
+        return result;
+    }
+
+    static bool cmp(string& a, string& b) {
+        return repeatString(a, 10) > repeatString(b, 10);
+    }
+
+    string largestNumber(vector<int>& nums) {
+        vector<string> tostr;
+        for (int n : nums) {
+            tostr.push_back(to_string(n));
+        }
+
+        sort(tostr.begin(), tostr.end(), cmp);
+        string answer = "";
+        for (string n : tostr) {
+            answer += n;
+        }
+
+        return answer;
+    }
+};
