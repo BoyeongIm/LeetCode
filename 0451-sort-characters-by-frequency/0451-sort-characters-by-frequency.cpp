3b1082fe7ad8b025002604631ee5ddba3ce0b572
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-18 16:19:30 +0900

    Time: 7 ms (36.96%), Space: 11 MB (73.31%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,31 +1,24 @@
-#include <string>
-#include <iostream>
-#include <map>
-#include <vector>
-
-using namespace std;
 class Solution {
 public:
-static bool cmp(pair<char, int>&a, pair<char, int>&b) {
-    return a.second > b.second;
-}
-
-string frequencySort(string s) {
-    map<char, int> frequency;
-    for (int i=0; i < s.size(); i++) {
-        if (frequency.find(s[i]) == frequency.end()) {
-            frequency[s[i]] = 1;
-        } else frequency[s[i]] ++;
+    static bool cmp(pair<char, int>& a, pair<char, int>& b) {
+        if (a.second == b.second) return a.first < b.first;
+        return a.second > b.second;
     }
-    
-    vector<pair<char, int>> freq2(frequency.begin(), frequency.end());
-    sort(freq2.begin(), freq2.end(), cmp);
-
-    string answer = "";
-    for (auto p : freq2) {
-        for (int j=0; j<p.second; j++) answer += p.first;
+    string frequencySort(string s) {
+        map<char, int> frequency;
+        for (int i=0; i<s.length(); i++) {
+            if (frequency.find(s[i])==frequency.end()) {
+                frequency[s[i]] = 1;
+            } else frequency[s[i]] ++;
+        }
+        
+        vector<pair<char, int>> freq2(frequency.begin(), frequency.end());
+        sort(freq2.begin(), freq2.end(), cmp);
+        
+        string s2 = "";
+        for (auto p : freq2) {
+            s2.append(p.second, p.first);
+        }
+        return s2;
     }
-
-    return answer;
-}
 };
