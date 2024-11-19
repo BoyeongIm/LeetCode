f535d3dffe931ba6186b7fd5c5e029d6c666a020
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-19 20:31:05 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def countBattleships(self, board):
+        row = len(board)
+        col = len(board[0])
+        visited = [[False] * col for _ in range(row)] 
+        s = 0
+        def dfs(x,y):
+            if x<0 or x>=row or y<0 or y>=col or board[x][y] == '.' or visited[x][y] == True:
+                return 0
+
+            visited[x][y] = True
+
+            dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+            dy = [0, 1, 1, 1, 0, -1, -1, -1]
+            for i in range(8):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                dfs(nx, ny)
+        count = 0
+        for i in range(row):
+            for j in range(col):
+                if board[i][j] == 'X' and visited[i][j] == False:
+                    dfs(i,j)
+                    count+=1
+        return count
+
