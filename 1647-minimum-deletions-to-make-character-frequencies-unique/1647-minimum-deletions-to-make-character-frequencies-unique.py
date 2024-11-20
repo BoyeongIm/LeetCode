909230ec08464859372294051b0356d45737d348
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-20 17:18:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minDeletions(self, s):
+        freq = dict()
+        unique_freq = set()
+        for c in s:
+            if c not in freq:
+                freq[c] = 1
+            else:
+                freq[c] += 1
+        if len(set(freq.values())) == len(freq):
+            return 0
+        sorted_freq = dict(sorted(freq.items(), key=lambda x:x[1], reverse=True))
+        for k,v in sorted_freq.items():
+            if v not in unique_freq:
+                unique_freq.add(v)
+            else:
+                while v in sorted_freq.values() and v in unique_freq:
+                    v -= 1
+                sorted_freq[k] = v
+                unique_freq.add(v)
+        return len(s) - sum(unique_freq)
