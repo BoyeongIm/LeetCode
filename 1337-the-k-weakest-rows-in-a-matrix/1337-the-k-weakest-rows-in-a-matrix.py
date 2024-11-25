16b191ae97804323aa13013775be75208141ac0b
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-25 18:58:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        frequency = []
+        for i in range(len(mat)):
+            frequency.append((sum(mat[i]), i))
+        frequency.sort(key=lambda x:(x[0],x[1]))
+        answer = []
+        for f in frequency:
+            answer.append(f[1])
+        return answer
