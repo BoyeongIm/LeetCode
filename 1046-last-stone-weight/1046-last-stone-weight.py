93a8f2084bed9a7ab0be57b6866f9b1cd7f4140b
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-25 16:40:03 +0900

    Time: 1 ms (15.53%), Space: 16.5 MB (62.71%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -4,12 +4,16 @@ class Solution:
             return 0
         if len(stones) == 1:
             return stones[0]
-
-        sorted_stones = sorted(stones, reverse=True)
-        first, second = sorted_stones[0], sorted_stones[1]
         
+        maxheap = []
+        for s in stones:
+            heapq.heappush(maxheap, -s)
+            
+        first = -heapq.heappop(maxheap)
+        second = -heapq.heappop(maxheap)
         new_w = first-second
         stones.append(new_w)
         stones.remove(first)
         stones.remove(second)
+
         return self.lastStoneWeight(stones)
