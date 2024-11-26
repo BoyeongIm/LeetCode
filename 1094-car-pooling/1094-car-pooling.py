37361f5eb2fa20e9f1bcbedb962a384b91b7b9a1
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-26 13:38:51 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,16 +1,12 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips.sort(key=lambda x : (x[1], x[2]))
-        count = [0] * 1001
-        for i in range(len(trips)):
-            people = trips[i][0]
-            dep = trips[i][1]
-            arr = trips[i][2]
-            count[dep] += people
-            count[arr] -= people
-        now = 0
-        for i in range(len(count)):
-            now += count[i]
-            if now > capacity:
+        heap = []
+        locations = [0]*1001
+        for passengers, dep, arr in trips:
+            heap.append([(dep, passengers), (arr, -passengers)])
+        heapify(heap)
+        while capacity>=0 and heap:
+            capacity += heappop(heap)[1][1]
+            if capacity < 0:
                 return False
         return True
