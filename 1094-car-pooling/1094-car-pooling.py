677ad95b698210b0f39ef019bc504667f68dc4d6
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-26 13:39:19 +0900

    Time: 2 ms (72.76%), Space: 17.2 MB (13.16%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,12 +1,17 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        heap = []
-        locations = [0]*1001
-        for passengers, dep, arr in trips:
-            heap.append([(dep, passengers), (arr, -passengers)])
-        heapify(heap)
-        while capacity>=0 and heap:
-            capacity += heappop(heap)[1][1]
-            if capacity < 0:
+        # 출발지 기준 우선 정렬 -> 도착지 기준 정렬 (오름차순)
+        trips.sort(key=lambda x : (x[1], x[2]))
+        # 각 인덱스에 해당하는 location일 때 몇명이 타고 몇명이 내리는지를 저장 !!!!
+        count = [0] * 1001
+
+        for people, dep, arr in trips:
+            count[dep] += people # dep에서 타는 인원 저장
+            count[arr] -= people # arr에서 내리는 인원 반영
+
+        now = 0 # 각 location을 돌면서 실시간 인원 조사
+        for i in range(len(count)):
+            now += count[i]
+            if now > capacity: # 실시간 인원이 capacity를 넘어가는 순간 False 반환
                 return False
         return True
