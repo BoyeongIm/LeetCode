9aaeb1164aaca9c2c279eb378c89737d5472b9e8
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-11-26 13:17:48 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key=lambda x : (x[1], x[2]))
+        count = [0] * 1001
+        for i in range(len(trips)):
+            people = trips[i][0]
+            dep = trips[i][1]
+            arr = trips[i][2]
+            count[dep] += people
+            count[arr] -= people
+        now = 0
+        for i in range(1, len(count)):
+            now += count[i]
+            if now > capacity:
+                return False
+        return True
