10766f3e65079010335c041748725dcbfccd6fc1
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-12-29 18:19:09 +0900

    Time: 2407 ms (9.71%), Space: 60.8 MB (37.86%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -1,27 +1,38 @@
-class Solution:
-    def validPath(self, G, source: int, destination: int) -> bool:
-        visited = [False for i in range(len(G))]
-        def dfs(u, v):
-            if u == v:
-                return True
-            if visited[u] == False:
-                visited[u] = True
-                for n in G[u]:
-                    if dfs(n, v): 
-                        return True
-            return False
-        return dfs(source, destination)
+class Solution(object):
+    def countPairs(self, n, edges):
+        adj = {i:[] for i in range(n)}
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        parents = [i for i in range(n)]
+        for u, v in edges:
+            if self.findparent(u, parents) != self.findparent(v, parents):
+                self.unite(u, v, parents)
+        count = {i:0 for i in range(n)}
+        for i in range(n):
+            parents[i] = self.findparent(i, parents)
+            count[parents[i]] +=1
+        print(parents)
+        print(count)
+        total = n*(n-1)/2
+        for c in count:
+            if count[c] > 1:
+                v = count[c]
+                total -= v*(v-1)/2
+        return total
 
-    def countPairs(self, n: int, edges: list[list[int]]) -> int:
-        G = {i : [] for i in range(n)}
-        
-        # an undirected graph: edge list to adjacency list
-        for e in edges:
-            G[e[0]].append(e[1])
-            G[e[1]].append(e[0])
-        count = 0
-        for i in range(n-1):
-            for j in range(i+1, n):
-                if not self.validPath(G, i, j):
-                    count += 1
-        return count
+    def findparent(self, node, parents):
+        if node != parents[node]:
+            parents[node] = self.findparent(parents[node], parents)
+        return parents[node]
+    
+    def unite(self, u, v, parents):
+        u_root = self.findparent(u, parents)
+        v_root = self.findparent(v, parents)
+        if u_root == v_root:
+            return
+        if u_root < v_root:
+            parents[v_root] = u_root
+        else:
+            parents[u_root] = v_root
+    
