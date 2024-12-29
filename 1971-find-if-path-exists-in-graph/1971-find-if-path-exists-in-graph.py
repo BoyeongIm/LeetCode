bd12de8e108402e903c2becc2fdfe1d89f6715c2
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-12-29 17:06:06 +0900

    Time: 623 ms (35.18%), Space: 131.8 MB (36.44%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,21 +1,18 @@
-class Solution(object):
-    def _DFTHelp(self, visited, node, destination, adj):
-        if node==destination:
-            return True
-        if not visited[node]:
-            visited[node]=True
-            for w in adj[node]:
-                if self._DFTHelp(visited, w, destination, adj):
-                    return True
-        return False
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = defaultdict(list)
+        for e in edges: # bi-directional graph
+            graph[e[0]].append(e[1])
+            graph[e[1]].append(e[0])
 
-    def validPath(self, n, edges, source, destination):
-        adj = {i:[] for i in range(n)}
-        for e in edges:
-            adj[e[0]].append(e[1])
-            adj[e[1]].append(e[0])
-        visited = {}
-        for i in range(n):
-            visited[i] = False
-        return self._DFTHelp(visited, source, destination, adj)
-        
+        visited = [False for i in range(n)]
+        def dfs(u, v):
+            if u == v:
+                return True
+            if visited[u] == False:
+                visited[u] = True
+                for n in graph[u]:
+                    if dfs(n, v): 
+                        return True
+            return False
+        return dfs(source, destination)
