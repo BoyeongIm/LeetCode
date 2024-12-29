8b0fe40075f1b51a01d88e11968fe548744602e9
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-12-29 20:56:08 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -1,17 +1,24 @@
-class Solution(object):
-    def countBattleships(self, board):
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        if not board:
+            return 0
+
+        # 행과 열의 길이
         row = len(board)
         col = len(board[0])
-        visited = [[False] * col for _ in range(row)] 
-        s = 0
-        def dfs(x,y):
-            if x<0 or x>=row or y<0 or y>=col or board[x][y] == '.' or visited[x][y] == True:
-                return 0
+        visited = [[False for _ in range(col)]] * row
 
+        def dfs(x, y):
+            # 행렬 범위를 벗어나거나 물(0)을 만나면 종료
+            if x < 0 or x >= row or y < 0 or y >= col or board[x][y] == '.' or visited[x][y] == True:
+                return
+
+            # 방문 처리
             visited[x][y] = True
 
-            dx = [-1, 0, 1, 0]
-            dy = [0, 1, 0, -1]
+            dx = [0, 1, 0, -1]
+            dy = [1, 0, -1, 0]
+
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
@@ -20,8 +27,7 @@ class Solution(object):
         count = 0
         for i in range(row):
             for j in range(col):
-                if board[i][j] == 'X' and visited[i][j] == False:
-                    dfs(i,j)
-                    count+=1
+                if board[i][j] == 'X' and visited[i][j] == False:  
+                    dfs(i, j)
+                    count += 1
         return count
-
