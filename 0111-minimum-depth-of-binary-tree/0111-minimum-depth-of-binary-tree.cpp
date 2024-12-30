013944fd9f56d7facb96d068a170b76515654fd0
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-12-30 14:08:23 +0900

    Time: 290 ms (5.42%), Space: 145.1 MB (88.44%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,9 +12,9 @@ public:
             for (int i=0; i<n; i++) {
                 TreeNode* curr = q.front();
                 q.pop();
-                if (!curr->left && !curr->right) return depth;
                 if (curr->left) q.push(curr->left);
                 if (curr->right) q.push(curr->right);
+                if (!curr->left && !curr->right) return depth;
                 }
             }
 
