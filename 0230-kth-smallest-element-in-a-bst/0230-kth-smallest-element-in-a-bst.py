7f520031983e41eddc3c66571e63d2cc543043a3
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-12-30 13:38:52 +0900

    Time: 9 ms (6.25%), Space: 21.1 MB (12.64%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,22 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-class Solution(object):
-    def _DFT_inorderHelp(self, currNode, k):
-        if not currNode:
-            return 
-        left = self._DFT_inorderHelp(currNode.left, k)
-        if left is not None:
-            return left
-        self.order += 1
-        if self.order == k:
-            return currNode.val
-        return self._DFT_inorderHelp(currNode.right, k)
-
-    def kthSmallest(self, root, k):
-        self.order = 0
-        return self._DFT_inorderHelp(root, k)
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        bst = []
+        def dfs(node, bst):
+            if not node:
+                return
+            if node.left:
+                dfs(node.left, bst)
+            bst.append(node.val)
+            if node.right:
+                dfs(node.right, bst)
+        dfs(root, bst)
+        print(bst)
+        return bst[k-1]
