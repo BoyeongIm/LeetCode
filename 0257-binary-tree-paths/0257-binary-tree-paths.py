f7de042352463c405bff8eb9c8c53d4fa6891dfc
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-12-30 15:28:01 +0900

    Time: 0 ms (100%), Space: 18 MB (7.95%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -1,17 +1,19 @@
-class Solution(object):
-    def binaryTreePaths(self, root):
-        if not root:
-            return []
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         paths = []
-        self.__help(root, paths, str(root.val))
+        def dfs(node, path):
+            if not node.left and not node.right:
+                if path not in paths:
+                    paths.append(path)
+            if node.left:
+                dfs(node.left, path + "->"+ str(node.left.val))
+            if node.right:
+                dfs(node.right, path + "->"+ str(node.right.val))
+        dfs(root, str(root.val))
         return paths
-    
-    def __help(self, currNode, paths, path):
-        if not currNode.left and not currNode.right:
-            if path not in paths:
-                paths.append(path)
-                return
-        if currNode.left:
-            self.__help(currNode.left, paths, path+"->"+str(currNode.left.val))
-        if currNode.right:
-            self.__help(currNode.right, paths, path+"->"+str(currNode.right.val))
