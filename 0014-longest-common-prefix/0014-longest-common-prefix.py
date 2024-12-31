bcbfeacfca6dd61f75a76023fa0cf8b9c4b4c72c
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2024-12-31 14:38:52 +0900

    Time: 4 ms (11.41%), Space: 17.9 MB (6.17%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,32 +1,13 @@
-class Solution(object):
-    # def longestCommonPrefix(self, strs):
-    #     lensorted = sorted(strs, key=len)
-    #     standard = lensorted[0]
-    #     count = 0
-    #     while count<len(standard):
-    #         for str in lensorted[1:]:
-    #             if standard[:count+1] != str[:count+1]:
-    #                 return standard[:count] 
-    #         count+=1
-    #     return standard
-    def longestCommonPrefix(self, strs):
-        lens = []
-        minlength = 9999
-        minlength_w = ""
-        count = 0
-        for s in strs:
-            lens.append(len(s))
-            if minlength > len(s):
-                minlength = len(s)
-                minlength_w = s
-        while len(minlength_w)>0:
-            for s in strs:
-                if s[:len(minlength_w)] != minlength_w:
-                    break
-                count +=1        
-            if count == len(strs):
-                return minlength_w
-            minlength_w = minlength_w[:-1]
-            count = 0
-        return minlength_w
-                        
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+        sorted_strs = sorted(strs, key = len)
+        shortest = sorted_strs[0]
+        length = 0
+        while length < len(shortest):
+            for word in sorted_strs:
+                if shortest[:length+1] != word[:length+1]:
+                    return shortest[:length]
+            length += 1
+        return shortest 
